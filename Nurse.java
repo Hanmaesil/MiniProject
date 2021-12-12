28f7d3ef2347ecd8c2162286670687211f8c47dd
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-12-12 23:29:03 +0900

    캐릭터 하위클래스

---
+++ Nurse.java
@@ -0,0 +1,27 @@
+
+public class Nurse extends Character {
+
+	String name = "간호사";
+	int health = 100; // 기본 체력
+	int resistance = 10 + 15; // 조기백신이 더해진 코로나 감염 저항력
+
+	@Override
+	public void selectCharcter() {
+		System.out.printf("체력 : %d\n", health);
+		System.out.printf("코로나 감염 저항력 : %d\n", resistance);
+
+	}
+
+	@Override
+	public void status(int health, int resistance) { //선택지로 증가 및 감소되는 수치를 받는 매개변수
+		this.health += health;
+		this.resistance += resistance;
+		System.out.println("===== 현재 스탯 =====");
+		System.out.println(name);
+		System.out.printf("체력 : %d\n", this.health);
+		System.out.printf("코로나 감염 저항력 : %d\n", this.resistance);
+		System.out.println("=================");
+	}
+
+
+}
